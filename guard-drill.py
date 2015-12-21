6d530db068d957b9f6f6988f9433ea22fa121d74
Johan Grahnen <johan.grahnen@gmail.com>
2015-12-21 10:52:48 -0800

    Tracking stationary distribution for the Markov chain.

--- guard-drill.py
+++ guard-drill.py
@@ -5,15 +5,25 @@ import time
 import pandas as pd
 import numpy as np
 
-# FIXME: BETTER TRANSITION PROBS (SOME GUARDS NEVER APPEAR!)
-
 # Read the (possibly poorly scaled) transition probabilities
 affinity_mat = pd.read_csv('guard-transition-probs.csv', index_col='Guard')
 
 # Re-scale them appropriately.
 trans_probs = affinity_mat / affinity_mat.sum()
 
+print "Transition probabilities between guards:"
 print trans_probs
+
+# Find an approximation to the stationary distribution
+n = 100
+stationary_dist = pd.Series(np.linalg.matrix_power(trans_probs.values, n)[:,0], index=trans_probs.index)
+
+print "\n\nLong-run probability of being in each guard:"
+print stationary_dist
+
+# FIXME: MIDDLE IRON DOOR IS NOT PROBABLE ENOUGH.
+
+exit(1)
 
 print "Push Ctrl+C to exit."
 next_action = np.random.choice(trans_probs.index)
