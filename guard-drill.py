48d252ff6d245230e473267460251f4a206300cc
Johan Grahnen <johan.grahnen@gmail.com>
2015-12-20 19:43:23 -0800

    Switched to Markov chains for guard transitions.

--- guard-drill.py
+++ guard-drill.py
@@ -2,22 +2,23 @@ import random
 import subprocess as sp
 import time
 
-low_guards = ["Middle boar's tooth", "Boar's tooth", "Middle iron door", 
-"Open iron door", "Tail guard"]
-middle_guards = ["Short guard", "Long guard", "Bicorn", "Crown guard"]
-high_guards = ["Women's guard", "Window guard", "Women's guard on the left"]
-steps = ["Accrehsehreh", "Dehccrehsehreh", "Mezzo volta", "Circle step", 
-"Volta stabile", "Tutti volta"]
+import pandas as pd
+import numpy as np
 
-action_types = [low_guards, middle_guards, high_guards, steps]
+# FIXME: BETTER TRANSITION PROBS (SOME GUARDS NEVER APPEAR!)
 
-# FIXME: MORE SENSIBLE ORDERS OF ACTIONS
-# FIXME: BETTER BLEND OF ACTION SETS
-# FIXME: INTRODUCE COMPLETELY RANDOM MODE (ANY ACTION AT ANY TIME)
+# Read the (possibly poorly scaled) transition probabilities
+affinity_mat = pd.read_csv('guard-transition-probs.csv', index_col='Guard')
+
+# Re-scale them appropriately.
+trans_probs = affinity_mat / affinity_mat.sum()
+
+print trans_probs
+
 print "Push Ctrl+C to exit."
+next_action = np.random.choice(trans_probs.index)
 while True:
-    current_type = random.choice(action_types)
-    for action in current_type:
-        print action
-        sp.call(['say', action])
-        time.sleep(2)
+    next_action = np.random.choice(trans_probs.index.values, p=trans_probs[next_action])
+    print next_action
+    sp.call(['say', next_action])
+    time.sleep(2)
